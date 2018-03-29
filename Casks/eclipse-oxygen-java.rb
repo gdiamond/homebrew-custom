d59b58679ffea4fbc49475f217160769700dafba
gdiamond <gdiamond@uen.org>
2018-03-29 14:12:47 -0600

    Create eclipse-oxygen-java.rb
    Added release name to Oxygen

---
+++ Casks/eclipse-oxygen-java.rb
@@ -0,0 +1,13 @@
+cask 'eclipse-oxygen-java' do
+  version '4.7.2,oxygen:2'
+  sha256 '45ab1f3ab53cf457c723818e2198e1b5d82479f4e8c7539a018dfb2934268d7d'
+
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-java-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
+  name 'Eclipse IDE for Java Developers'
+  homepage 'https://eclipse.org/'
+
+  depends_on macos: '>= :leopard'
+
+  # Renamed to avoid conflict with other Eclipse.
+  app 'Eclipse.app', target: 'Eclipse Oxygen Java.app'
+end
