2e8235307ab156a42f76db669d169ca624b8e8ea
Gary Diamond <gdiamond@uen.org>
2018-04-16 11:59:11 -0600

    added aqua data studio

---
+++ Casks/aqua-data-studio@18.5.rb
@@ -0,0 +1,10 @@
+cask 'aqua-data-studio' do
+  version '18.5.0'
+  sha256 '744955217e65c21d64ed0b7f5974e6cb75fb66ef2e2bde59c8ad384b7169d140'
+
+  url "http://downloads.aquafold.com/v#{version.major_minor}.0/osx/ads-osx-#{version}.tar.gz"
+  name 'Aquafold Aqua Data Studio'
+  homepage 'http://www.aquafold.com/aquadatastudio.html'
+
+  app 'Aqua Data Studio.app'm , target: 'Aqua Data Studio 18.5.app'
+end
